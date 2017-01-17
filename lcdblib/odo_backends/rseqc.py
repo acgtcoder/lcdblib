791f7114351e95bdbb4f07d2dc8de5d66cf4c829
Justin Fear <justin.m.fear@gmail.com>
2017-01-17 09:45:38 -0500

    Adds the remainder of the rseqc odo resources.

--- lcdblib/odo_backends/rseqc.py
+++ lcdblib/odo_backends/rseqc.py
@@ -3,12 +3,20 @@ import re
 from odo import resource
 import pandas as pd
 
-def ParsingError(Exception):
+
+class ParsingError(Exception):
     pass
 
+
 @resource.register('.*infer_experiment\.txt', priority=20)
-def resource(uri, **kwargs):
-    """odo resource to parse infer_experiment results."""
+def resource_infer_experiment(uri, **kwargs):
+    """odo resource to parse infer_experiment results.
+
+    Note
+    ----
+    The infer experiment file must be named `*infer_experiment.txt`.
+
+    """
     header = [
         'Fraction of reads failed to assign',
         'Fraction of reads on same strand as gene',
@@ -51,3 +59,57 @@ def resource(uri, **kwargs):
             pass
 
         raise ParsingError('File did not conform to SE or PE read pattern.')
+
+@resource.register('.*geneBody_coverage\.txt', priority=20)
+def resource_geneBody_coverage(uri, **kwargs):
+    """odo resource to parse geneBody_coverage results.
+
+    Note
+    ----
+    The gene body coverage file must be named `*geneBody_coverage.txt`.
+
+    """
+    df = pd.read_csv(uri, sep='\t', index_col=0)
+    df.index = [x.replace('.prealn', '') for x in df.index]
+    df.index.name = 'sample_id'
+    return df
+
+@resource.register('.*tin\.txt', priority=20)
+def resource_tin(uri, **kwargs):
+    """odo resource to parse tin results.
+
+    Note
+    ----
+    The gene body coverage file must be named `*tin.txt`.
+    """
+    df = pd.read_csv(uri, sep='\t', index_col=0)
+    df.index = [x.replace('.prealn.bam', '') for x in df.index]
+    df.index.name = 'sample_id'
+    return df
+
+@resource.register('.*bam_stat\.txt', priority=20)
+def resource_tin(uri, **kwargs):
+    """odo resource to parse bam_stat results.
+
+    Note
+    ----
+    The gene body coverage file must be named `*bam_stat.txt`.
+    """
+
+    header = ['Total_records', 'QC_failed', 'Optical_PCR_duplicate',
+              'Non_primary_hits', 'Unmapped_reads', 'non-unique',
+              'unique', 'Read_1', 'Read_2', 'Reads_map_plus', 'Reads_map_minus', 'NonSplice_reads',
+              'Splice_reads', 'Reads_mapped_in_proper_pairs',
+              'ProperPaired_reads_map_to_different_chrom']
+
+    with open(uri, 'r') as fh:
+        values = []
+        for row in fh:
+            row = row.strip()
+            try:
+                value = re.match(r'.*[\s\:](\d+)', row).groups()[0]
+                values.append(int(value))
+            except:
+                pass
+
+        return pd.Series(data=values, index=header)
--- tests/test_odo_backends.py
+++ tests/test_odo_backends.py
@@ -2,9 +2,10 @@
 
 import os
 import pytest
+from math import ceil
 from odo import odo, resource
 import pandas as pd
-from lcdblib.odo_backends import rseqc
+from lcdblib.odo_backends.rseqc import *
 
 
 @pytest.fixture
@@ -44,3 +45,76 @@ def test_infer_experiment_pe(infer_experiment_pe):
     assert dat[1] == 0.0126
     assert dat[2] == 0.7345
     assert dat[3] == 'PairEnd'
+
+@pytest.fixture
+def geneBody_coverage(tmpdir):
+    data = (
+        'Percentile	1	2	3	4	5	6	7	8	9	10\n'
+        'SRR9999999.prealn	46.0	70.0	115.0	154.0	224.0	246.0	275.0	343.0	418.0	457.0')
+    fn = os.path.join(str(tmpdir), 'test_geneBody_coverage.txt')
+    with open(fn, 'w') as fh:
+        fh.write(data)
+    return fn
+
+def test_geneBody_coverage(geneBody_coverage):
+    dat = resource(geneBody_coverage)
+    assert dat.iloc[0, 0] == 46.0
+    assert dat.columns[0] == '1'
+    assert dat.index[0] == 'SRR9999999'
+    assert dat.index.name == 'sample_id'
+
+
+@pytest.fixture
+def tin(tmpdir):
+    data = (
+        'Bam_file	TIN(mean)	TIN(median)	TIN(stdev)\n'
+        'SRR9999999.prealn.bam	25.6425321019	22.2814373475	17.9599093927')
+    fn = os.path.join(str(tmpdir), 'test_tin.txt')
+    with open(fn, 'w') as fh:
+        fh.write(data)
+    return fn
+
+def test_tin(tin):
+    dat = resource(tin)
+    assert ceil(dat.iloc[0, 0]) == 26
+    assert dat.columns[0] == 'TIN(mean)'
+    assert dat.index[0] == 'SRR9999999'
+    assert dat.index.name == 'sample_id'
+
+
+@pytest.fixture
+def bam_stat(tmpdir):
+    data = (
+        'Load BAM file ...  Done\n'
+        '\n'
+        '#==================================================\n'
+        '#All numbers are READ count\n'
+        '#==================================================\n'
+        '\n'
+        'Total records:                          3759051\n'
+        '\n'
+        'QC failed:                              0\n'
+        'Optical/PCR duplicate:                  0\n'
+        'Non primary hits                        0\n'
+        'Unmapped reads:                         0\n'
+        'mapq < mapq_cut (non-unique):           0\n'
+        '\n'
+        'mapq >= mapq_cut (unique):              3759051\n'
+        'Read-1:                                 1892634\n'
+        'Read-2:                                 1866417\n'
+        '''Reads map to '+':                       1880063\n'''
+        '''Reads map to '-':                       1878988\n'''
+        'Non-splice reads:                       3484515\n'
+        'Splice reads:                           274536\n'
+        'Reads mapped in proper pairs:           3474678\n'
+        'Proper-paired reads map to different chrom:0')
+
+    fn = os.path.join(str(tmpdir), 'test_bam_stat.txt')
+    with open(fn, 'w') as fh:
+        fh.write(data)
+    return fn
+
+def test_bam_stat(bam_stat):
+    dat = resource(bam_stat)
+    assert dat[0] == 3759051
+    assert dat[-1] == 0
